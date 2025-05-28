27dce5c993b948d62db3c9fb2a856ffb3d042a4f
Tyler Careless <tyler444c@gmail.com>
2025-05-28 14:20:02 -0400

    Bueno, now need to extend from parent.html and use bootstrap

--- app/plant/views/plant_spine_views.py
+++ app/plant/views/plant_spine_views.py
@@ -1,11 +1,29 @@
+# plant/views.py
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo     # Python 3.9+ built-in; for older Pythons use `import pytz`
 from django.shortcuts import render
 from plant.models.plantSpine_models import PlantSpine
 
 def plant_blueprint(request):
     """
-    Displays all PlantSpine records in a table.
+    Displays all PlantSpine records in a table, converting the
+    cycle_time_effective_date epoch into an EST/EDT datetime.
     """
     records = PlantSpine.objects.all()
+
+    # set up the Eastern timezone
+    eastern = ZoneInfo('America/New_York')
+    for rec in records:
+        ts = rec.cycle_time_effective_date
+        # if you actually store milliseconds, convert to seconds:
+        if ts > 10**12:
+            ts = ts / 1000.0
+        # interpret as UTC then convert to Eastern
+        rec.effective_dt = (
+            datetime.fromtimestamp(ts, tz=timezone.utc)
+                    .astimezone(eastern)
+        )
+
     return render(request, 'plant/plant_blueprint.html', {
         'records': records,
     })
