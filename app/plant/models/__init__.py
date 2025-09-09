10aa0166f660863ee59793cb28fbec404bdf39b4
Tyler Careless <tyler444c@gmail.com>
2025-09-09 11:42:59 -0400

    Okay I think I've now migrated the testing tables. We can now edit the views to point to these now

--- app/plant/models/__init__.py
+++ app/plant/models/__init__.py
@@ -2,3 +2,4 @@
 from .maintenance_models import MachineDowntimeEvent
 from .tempsensor_models import TempSensorEmailList
 from .absentee_models import AbsenteeReport
+from .maintenance_test_models import *
